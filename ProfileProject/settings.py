52caf7066e645617ebcd0479b7bebb35271ff073
Mr.Flava <thatelitemaili33t@gmail.com>
2021-01-18 23:25:15 +0200

    deleted SECRET_KEY

--- ProfileProject/settings.py
+++ ProfileProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8#w3h*^dqx!0s*149jemb*^taj0d!+(9&@-pz=m3vbr961x7+i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
